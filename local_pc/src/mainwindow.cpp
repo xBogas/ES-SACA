1697d29acebdfe23067431d6fb9991583835087f
Cassien <up201403187@fe.up.pt>
2023-03-27 17:54:12 +0100

    Adicionei imagens e criei a página main.

--- local_pc/src/mainwindow.cpp
+++ local_pc/src/mainwindow.cpp
@@ -6,6 +6,13 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QPixmap owl("local_pc/resource/img/744px-Muskets.svg.png");
+    ui->label->setPixmap(owl);
+    QPixmap impist("/home/cassienc/ES-S/ES-SACA/local_pc/resource/img/crossed-pistols-on-the-black-background.gif");
+    ui->label_2->setPixmap(impist);
+    QPixmap imrif("/home/cassienc/ES-S/ES-SACA/local_pc/resource/img/744px-Muskets.svg.png");
+    ui->label_3->setPixmap(imrif);
 }
 
 MainWindow::~MainWindow()
