59f8317cbe8265b3f7219e684cdb29943ce63f66
João Magalhães <joaorm.jrm2014@gmail.com>
2023-04-06 01:07:05 +0100

    solved problem of gui not showing while client or server code were running

--- central_pc/src/central_pc.cpp
+++ central_pc/src/central_pc.cpp
@@ -17,10 +17,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    a.exec();
 
     server.join();
 
-    return a.exec();
+    return 0;
 }
 
 void server_thread(){
--- local_pc/src/local_pc.cpp
+++ local_pc/src/local_pc.cpp
@@ -17,10 +17,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    a.exec();
 
     client.join();
 
-    return a.exec();
+    return 0;
 }
 
 void client_thread(){
