4d5635e9a15b21a90f2b520aa9444ec83862c974
xBogas <119903183+xBogas@users.noreply.github.com>
2023-04-08 17:08:26 +0100

    vision: Filter center

--- local_pc/vison_analysis/src/Detector.hpp
+++ local_pc/vison_analysis/src/Detector.hpp
@@ -19,6 +19,15 @@ private:
 	void
 	getCenter();
 	
+	std::tuple<double,double>
+	mean(const std::vector<cv::Vec3f>& data);
+
+	std::tuple<double,double>
+	standartDeviation(const std::vector<cv::Vec3f>& data, double mean_x = 0, double mean_y = 0);
+
+	std::tuple<int, int>
+	rejectOutliers(const std::vector<cv::Vec3f>& data, int threshold);
+
 	cv::VideoCapture m_camara;
 	cv::Mat m_image;
 };
