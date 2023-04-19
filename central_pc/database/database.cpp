68ca41b0f1d1ae6037bae496f147100c87c3d493
ines <m.inesimoes01@gmail.com>
2023-04-19 16:49:22 +0100

    transfered functions from demo to database

--- central_pc/database/database.cpp
+++ central_pc/database/database.cpp
@@ -1,20 +1,61 @@
 #include "database.h"
 
-#include <QSqlError>
-#include <QSqlQuery>
-#include <QDebug>
+Database::Database() {
+    const string DbHostIP="127.0.0.1";
+    const string DbName="es_saca";
+    const string DbUser="postgres";
+    const string DbPassword="equipaE_saca";
+    const string DbPort="5432";
 
-Database::Database(QObject *parent) : QObject(parent)
-{
-    m_db = QSqlDatabase::addDatabase("QPSQL");
-    m_db.setHostName("localhost");
-    m_db.setDatabaseName("shooting_db");
-    m_db.setUserName("postgres");
-    m_db.setPassword("mypassword");
-    m_db.setPort(5432);
+    static connection conn("host="+DbHostIP+" dbname="+DbName+" user="+DbUser+" password="+DbPassword+" port="+DbPort);
+
+    if (conn.is_open()) {
+        std::cout << "Successful Connection to " << conn.dbname() << std::endl;
+    } else {
+        std::cout << "Failed Connection" << std::endl;
+        throw std::runtime_error("Can't open database");
+    }
 }
 
-bool Database::login(const QString &username, const QString &password)
+
+Database::~Database() {
+    conn->disconnect();
+    delete conn;
+}
+
+/*int db_INSERT_Athlete(int licenseid, string name, string gender, string nationality, int age, string club){
+
+    std::__cxx11::basic_string<char> sql;
+
+    try{
+        work W(*conn);
+
+        sql = "INSERT INTO \"Athlete\" (licenseid, name, gender, nationality, age, club) VALUES (" + to_string(licenseid) + ", '" + name + "', '" + gender + "', '" + nationality + "', " + to_string(age) +", '" + club + "')";
+
+        W.exec(sql.c_str());
+        W.commit();
+        cout << "Updated successfully" << endl;
+
+
+    }catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+      return 1;
+    }
+
+    return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+/*bool Database::login(const QString &username, const QString &password)
 {
     if (!m_db.open()) {
         qDebug() << "Failed to open database";
@@ -69,4 +110,4 @@ bool Database::insertScore(const QString &athlete, const QString &competition, i
     query.bindValue(":competition", competition);
     query.bindValue(":score", score);
     return query.exec();
-}
+}*/
