9ae1aa568116c643f7fa5f91fca78bc047b9ec2b
ES-SACA <es-saca@outlook.com>
2023-04-20 15:40:59 +0100

    vision: fixed include

--- local_pc/vison_analysis/src/image_alignment.hpp
+++ local_pc/vison_analysis/src/image_alignment.hpp
@@ -3,14 +3,12 @@
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/xfeatures2d.hpp>
-#include <opencv2/features2>
+#include <opencv2/features2d.hpp>
 
 using namespace std;
 using namespace cv;
 using namespace cv::xfeatures2d;
  
-const int MAX_FEATURES = 1000;
-const float GOOD_MATCH_PERCENT = 0.15f;
 
 void alignImages(Mat &im1, Mat &im2, Mat &im1Reg, Mat &h);
 
