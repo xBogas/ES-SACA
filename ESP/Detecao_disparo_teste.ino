5dfd00c296ff9b7ccdd9ce4f12e4b9eddf977c9c
João Magalhães <joaorm.jrm2014@gmail.com>
2023-05-25 16:34:03 +0100

    communication between ESP and local_pc working properly

--- ESP/Detecao_disparo_teste.ino
+++ ESP/Detecao_disparo_teste.ino
@@ -22,7 +22,7 @@ typedef struct {
 
 fsm_t fsm0;
 
-String message = "";
+String message;
 
 // Set new state
 void set_state(fsm_t& fsm, int new_state)
@@ -39,7 +39,7 @@ WiFiServer server(port);
 WiFiClient client;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(base,OUTPUT); 
   
   // Initialize state machines
@@ -59,60 +59,59 @@ void loop() {
     client = server.available();
   }
 
-   // Update tis for all state machines
-  unsigned long cur_time = millis();   // Just one call to millis()
-  fsm0.tis = cur_time - fsm0.tes;
-
-
   if (client.connected()) {
-    
+
+    if(client.available()) 
+      Serial.println("available");
+
     while (client.available()) {
-    Serial.println("available");
       //String message = client.readStringUntil('\n');
       //Serial.println("Received message: " + message);
+
+      // Update tis for all state machines
+      unsigned long cur_time = millis();   // Just one call to millis()
+      fsm0.tis = cur_time - fsm0.tes;
+
+      // read sensor
+      sensorValue = digitalRead(13);
   
       if(sensorValue && fsm0.state == 0){
-        fsm0.new_state = 1;
-        #ifdef __DEBUG__
-          Serial.println("disparo");
-        #endif  
+        set_state(fsm0, 1); 
+
+        Serial.println("disparo");
       }
-      else if(fsm0.state == 1 && message == "anda"){
-        fsm0.new_state = 2;
+      else if(fsm0.state == 1){
+        set_state(fsm0, 2); 
+
         message = "";
       }
       else if(fsm0.state == 2 && fsm0.tis > 2000){
-        fsm0.new_state = 3;
-        #ifdef __DEBUG__
-          Serial.println("Para motor");
-        #endif
-      }
-      else if(fsm0.state == 3 && message == "parou"){
-        fsm0.new_state = 0;
-        message = "";
+        set_state(fsm0, 0); 
+
+        Serial.println("Para motor");
       }
 
+      // update outputs
       if(fsm0.state == 0){
-        sensorValue = digitalRead(13);
-        digitalWrite(base, LOW);
+        //digitalWrite(base, LOW);
+
+        Serial.println("actual state -> 0");
       }
       else if(fsm0.state == 1){
+        Serial.println("actual state -> 1 (inicio)");
+
         client.println("disparo");
         message = client.readStringUntil('\n');
+        Serial.println("Received message: " + message);
+
+        Serial.println("actual state -> 1 (fim)");
       }
       else if(fsm0.state == 2){
-        digitalWrite(base, HIGH);
+        //digitalWrite(base, HIGH);
+
+        Serial.println("actual state -> 2");
       }
-      else if(fsm0.state == 3){
-        digitalWrite(base, LOW);
-        client.println("para");
-        message = client.readStringUntil('\n');
-      }
-  
-      // Update the states
-      set_state(fsm0, fsm0.new_state); 
       
-      client.println("Message received"); // Send a response back to the client
       delay(10);
     }
   }
