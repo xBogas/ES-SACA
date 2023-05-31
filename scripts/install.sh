36dc8fd3d20d0e3ae26c7558a2d54a69afb10cb1
xBogas <joaonunopereirab@gmail.com>
2023-05-31 15:35:25 +0100

    Scripts: install script

---
+++ scripts/install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# opencv
+# eigen
+# boost
+
+# postgre sql for central
+
+# go to home/user directory
+cd /home/saca
+
+# create download folder for saca dependencies
+mkdir saca_downloads && cd saca_downloads
+
+sudo apt-get update -y
+
+# OpenCV
+
+# Install opencv dependencies
+sudo apt-get install -y cmake g++ wget unzip git
+
+mkdir opencv && cd opencv
+wget -O opencv.zip https://github.com/opencv/opencv/archive/4.x.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.x.zip
+
+unzip opencv.zip
+unzip opencv_contrib.zip
+
+mkdir -p build && cd build
+
+cmake -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib-4.x/modules ../opencv-4.x
+cmake --build .
+
+sudo make install
+
+return 0
